a26c52fa3525d9914ad886203298242dceb7df02
Aiman SIngh <145761115+Aimans30@users.noreply.github.com>
2024-03-21 14:13:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css'; // assuming you have some CSS file for styling
+import HigherOrderComponent from './assets/Components/HigherOrderComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderComponent />
+    </div>
+  );
+}
+
+export default App;
